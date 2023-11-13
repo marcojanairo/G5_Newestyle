7a6982020e84091475cc9c52d5843f1e4d28b791
ttgksy <sanotomoya@Macbook-T.local>
2023-11-13 12:51:31 -0800

    added logo and companyName

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import clothes from './clothesData.js'; // import data file
 
+function ClothingItem(props) {
+
+  const imageStyle = {
+    width: '250px',
+    height: '250px',
+  }
+  
+  return (
+    <div className="clothing-item">
+      <h2>{props.name}</h2>
+      <p>Gender: {props.gender}</p>
+      <p>Item description: {props.desc}</p>
+      <img src={props.url} alt={props.name} style={imageStyle}/>
+    </div>
+  )
+}  
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="center-container">
+          <img src="./logo_Newestyle.png" alt="logo_Newestyle" 
+          style={{ width: '100px', height: '100px' }}  />
+        </div>
+        <h1>Welcome to Newestyle</h1>
+        <div className="button-container">
+        <button >T-Shirts</button><button>Jackets</button><button>Pants</button>
+        </div>
       </header>
+      <div className="container">
+      {clothes.map((clothing) => (
+        <ClothingItem
+          key={clothing.id}
+          name={clothing.name}
+          gender={clothing.gender}
+          desc={clothing.desc}
+          url={clothing.url}
+        />
+      ))}
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/clothesData.js
@@ -0,0 +1,60 @@
+const clothes = [
+    {
+      id: '1',
+      name: 'Zeus',
+      gender: 'male',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' //https://www.pexels.com/photo/selective-focus-photography-white-button-up-collared-top-297933/
+    },
+    {
+      id: '2',
+      name: 'Apollo',
+      gender: 'female',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'// 'https://www.pexels.com/photo/assorted-clothes-996329/' 
+    },
+    {
+      id: '3',
+      name: 'Hades',
+      gender: 'neutral',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/men-s-white-shirt-428340/' 
+    },
+    {
+      id: '4',
+      name: 'Hermes',
+      gender: 'neutral',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/man-wearing-pink-tee-shirt-leaning-on-wall-while-head-down-1484807/'
+    },
+    {
+      id: '5',
+      name: 'Poseidon',
+      gender: 'female',
+      desc: 'The planet Jupiter is the fifth planet out from the Sun, and is two and a half times more massive than all the other planets in the solar system combined. It is made primarily of gases and is therefore known as a "gas giant".',
+      url: 'https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/man-in-white-and-blue-plaid-dress-shirt-769749/' 
+    },
+    {
+      id: '6',
+      name: 'Hephaestus',
+      gender: 'male',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/2205839/pexels-photo-2205839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/close-up-photo-of-person-wearing-black-shirt-2205839/'
+    },
+    {
+      id: '7',
+      name: 'Athena',
+      gender: 'female',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/1043148/pexels-photo-1043148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/man-wearing-blue-button-up-dress-shirt-1043148/' 
+    },
+    {
+      id: '8',
+      name: 'Dionysus',
+      gender: 'female',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae orci eget libero varius porttitor. Nullam feugiat congue risus et aliquet. Proin eu cursus est.',
+      url: 'https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'//'https://www.pexels.com/photo/man-in-white-crew-neck-t-shirt-standing-on-bridge-1261422/' 
+    },
+];
+  
+export default clothes;
