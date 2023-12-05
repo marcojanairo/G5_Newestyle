30593de18ba2901d0747a9eca1127fee0592803a
Marco <marcojanairo@gmail.com>
2023-12-04 17:21:36 -0800

    updated URI

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI;
 
-const uri = "mongodb+srv://FE_Prep_User:12345@feprepcluster.37d7plq.mongodb.net/FeedbackForm_Project";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
